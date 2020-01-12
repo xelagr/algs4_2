deef6411aa708496ee9f1ea606f5e611f74bbe56
snezhok <skoknaeva@gmail.com>
2020-01-11 23:47:02 -0500

    base Graph implementation and test

--- src/java/io/xelagr/algs4/graph/Graph.java
+++ src/java/io/xelagr/algs4/graph/Graph.java
@@ -2,23 +2,39 @@ package io.xelagr.algs4.graph;
 
 import edu.princeton.cs.algs4.In;
 
-public class Graph {
-    private int V;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Graph {
+    private final int V;
+    private int E;
+    private List<Integer>[] adj;
+
+    @SuppressWarnings("unchecked")
     public Graph(int V) {
         this.V = V;
+        adj = new ArrayList[V];
+        for (int i = 0; i < adj.length; i++) {
+            adj[i] = new ArrayList<>();
+        }
     }
 
     public Graph(In in) {
-
+        this.V = in.readInt();
+        int edgeCount = in.readInt();
+        for (int i = 0; i < edgeCount; i++) {
+            addEdge(in.readInt(), in.readInt());
+        }
     }
 
     public void addEdge(int v, int w) {
-
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
     }
 
     public Iterable<Integer> adj(int v) {
-        return null;
+        return adj[v];
     }
 
     int V() {
@@ -26,11 +42,19 @@ public class Graph {
     }
 
     int E() {
-        return 0;
+        return E;
     }
 
     @Override
     public String toString() {
-        return "Graph{}";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < adj.length; i++) {
+            sb.append(i).append(":");
+            for (Integer v: adj[i]) {
+                sb.append(" ").append(v);
+            }
+            sb.append("\r\n");
+        }
+        return sb.toString();
     }
 }
---
+++ src/java/io/xelagr/algs4/graph/GraphTest.java
@@ -0,0 +1,13 @@
+package io.xelagr.algs4.graph;
+
+public class GraphTest {
+    public static void main(String[] args) {
+        Graph g = new Graph(10);
+        g.addEdge(0, 1);
+        g.addEdge(5, 7);
+        g.addEdge(7, 1);
+        System.out.println("g.V(): " + g.V());
+        System.out.println("g.E(): " + g.E());
+        System.out.println(g);
+    }
+}
